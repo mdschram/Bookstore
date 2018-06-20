299b120b2bb8e3fe27ea240c8fd20715bbb42b0e
Michiel Schram <mdschram@hotmail.com>
2018-06-20 12:15:11 +0200

    Bookstore searchbar working

---
+++ main.js
@@ -0,0 +1,63 @@
+function start() {
+    console.log(1);
+
+    var fetchConfig =
+        fetch("https://api.myjson.com/bins/1h3vb3", {
+            method: "GET"
+        })
+        .then(onDataFetched)
+        .catch(onDataFetchFailed);
+
+    console.log(3);
+}
+
+function onDataFetched(response) {
+    response.json()
+        .then(onConversionToJsonSuccessful)
+        .catch(onConversionToJsonFailed);
+    console.log(2);
+}
+
+function onDataFetchFailed(error) {
+    console.log("I have failed in life.", error);
+}
+
+function onConversionToJsonSuccessful(json) {
+    console.log("success!!!!", json);
+    serverData = json;
+    testBooks()
+}
+
+function onConversionToJsonFailed() {
+    //    console.log("Not a json mate!");
+}
+
+start()
+
+var main = new Vue({
+    el: '#main',
+    data: {
+        books: [],
+        search: "",
+    },
+    computed: {
+        filteredBooks: function () {
+            return this.books.filter((book) => {
+                return book.titulo.match(this.search) || book.descripcion.match(this.search)
+            })
+        }
+
+    },
+    methods: {
+        clicked: function () {
+            console.log(book.titulo)
+        }
+    }
+})
+
+function testBooks() {
+    var books = serverData.books
+    for (i = 0; i < books.length; i++) {
+        main.books.push(books[i])
+    }
+}
